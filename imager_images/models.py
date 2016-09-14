51217ffaecbadb444399413051e59e76509d70a5
crashtack <crashtack@gmail.com>
2016-09-14 16:02:56 -0700

    added models for imager_images

---
+++ imager_images/models.py
@@ -0,0 +1,64 @@
+from __future__ import unicode_literals
+import uuid
+from django.db import models
+from django.conf import settings
+from django.utils.encoding import python_2_unicode_compatible
+# Create your models here.
+
+
+class Photo(models.Manager):
+    '''A Photo belonging to a usr'''
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,
+                                on_delete=models.CASCADE)
+    albums = models.ManyToManyField('Album')
+    card_number = models.UUIDField(primary_key=True,
+                                   default=uuid.uuid4,
+                                   editable=False)
+    file = models.ImageField(upload_to=user_directory_path(instance, filename),
+                             height_field=None,
+                             width_field=None, max_length=100)
+    title = models.CharField("Title", max_length=255, blank=True)
+    height_field = models.IntegerField("Height", blank=True)
+    width_field = models.IntegerField("Width", blank=True)
+    latitude = models.IntegerField("Latitude", blank=True)
+    longitude = models.IntegerField("Longitude", blank=True)
+    camera = models.CharField("Camera", max_length=64, blank=True)
+    lens = models.CharField("Lens", max_length=64, blank=True)
+    focal_length = models.CharField("Focal Length", max_length=32, blank=True)
+    iso = models.IntegerField("ISO", blank=True)
+    shutter_speed = models.IntegerField("Shutter Speed", blank=True)
+    appature = models.CharField("Title", max_length=64, blank=True)
+    description = models.CharField("Title", max_length=255, blank=True)
+    date_created = models.DateField('Date Created', auto_now_add=True)
+    date_modified = models.DateField('Date Modified', auto_now=True)
+    date_pub = models.DateField('Date Published', editable=True, blank=True)
+    published = models.CharField(max_length=64,
+                                 choices=[('private', 'private'),
+                                          ('shared', 'shared'),
+                                          ('public', 'public')])
+
+    def user_directory_path(instance, filename):
+        ''' file will be uploaded to MEDIA_ROOT/user_<id>/%Y-%m-%d<filename>'''
+        return 'user_{0}/{1}'.format(instance.user.id, filename)
+
+    def __str__(self):
+        return self.file
+
+    class Meta:
+        ordering = ('date_created',)
+
+
+class Album(models.Manger):
+    '''An Album of Photos'''
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,
+                                on_delete=models.CASCADE)
+    title = models.CharField("Title", max_length=255, blank=True)
+    description = models.CharField("Description", max_length=255, blank=True)
+    cover_photo = models.CharField("Cover Photo", max_length=255, blank=True)
+    date_created = models.DateField('Date Created', auto_now_add=True)
+    date_modified = models.DateField('Date Modified', auto_now=True)
+    date_pub = models.DateField('Date Published', editable=True, blank=True)
+    published = models.CharField(max_length=64,
+                                 choices=[('private', 'private'),
+                                          ('shared', 'shared'),
+                                          ('public', 'public')])
