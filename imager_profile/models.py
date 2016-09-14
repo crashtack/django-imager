b3bf247fadc54775913351ddfdb0fbb5f215226f
crashtack <crashtack@gmail.com>
2016-09-14 10:55:28 -0700

    updated during lecture

--- imager_profile/models.py
+++ imager_profile/models.py
@@ -18,12 +18,23 @@ def make_sure_user_profile_is_added_on_user_created(sender, **kwargs):
         loger.debut('User_Profile created {}'.format(up))
 
 
+class PatronProfileManager(models.Manager):
+    '''returns a queryset pre-filters to only active profiles'''
+    class Meta:
+        model = "PatronProfile"
+
+    def get_queryset(self):
+        qs = super(PatronProfileManager, self).get_queryset()
+        return qs.filter(user__is_active=True)
+
+
 @python_2_unicode_compatible
 class Photographer(models.Model):
     user_uuid = models.UUIDField(primary_key=True,
-                                   default=uuid.uuid4,
-                                   editable=False)
-    user = models.OneToOneField(settings.AUTH_USER_MODEL)
+                                 default=uuid.uuid4,
+                                 editable=False)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,
+                                on_delete=models.CASCADE)
     has_portfolios = models.BooleanField()
     portfolio_url = models.CharField('p')
 
@@ -31,26 +42,44 @@ class Photographer(models.Model):
         fn = self.user.get_full_name().strip() or self.user.get_username()
         return "{}: {}".format(fn, self.user_uuid)
 
-    def active():
-        pass
+    @property
+    def is_active(self):
+        return self.user.is_active
+
+    objects = models.Manager()
+    active = PatronProfileManager()
+
+    # don't do it this way, use the Manager Method (PratonProfileManager)
+    # @classmethod
+    # def active(cls):
+    #     return cls.objects.filter(user__is_active__exactly=True)
 
 
 class Address(models.Model):
+    photographer_profile = models.ForeignKey(
+        Photographer,
+        on_delete=models.CASCADE,
+        primary_key=True,
+        related_name='Addresses',  # TODO: look this up and set it
+    )
     address_1 = models.CharField('Street Address 1',
                                  max_length=255,
-                                 blank=True,
-                                 null=True)
+                                 blank=True,  # it's valid to be empty in Python
+                                 null=True)   # allow it to be empty in DB
     address_2 = models.CharField('Street Address 2',
                                  max_length=255,
                                  blank=True,
                                  null=True)
-    city = models.CharField(max_length=128,
-                                 blank=True,
-                                 null=True)
-    state = models.CharField(max_length=2,
-                                 blank=True,
-                                 null=True)
-    post_code = models.CharField(max_length=7,
+    city = models.CharField('City',
+                            max_length=128,
+                            blank=True,
+                            null=True)
+    state = models.CharField('State',
+                             max_length=2,
+                             blank=True,
+                             null=True)
+    post_code = models.CharField('Zip Code',
+                                 max_length=7,
                                  blank=True,
                                  null=True)
 
@@ -59,3 +88,13 @@ class Equipment(models.Model):
     equipment_type = models.CharField(max_length=200, blank=True, null=True)
     make = models.CharField(max_length=200, blank=True, null=True)
     model = models.CharField(max_length=200, blank=True, null=True)
+
+
+class SocialMedia(models.Model):
+    photographer_profile = models.ForeignKey(
+        Photographer,
+        on_delete=models.CASCADE,
+        primary_key=True,
+        related_name='Addresses',  # TODO: look this up and set it
+    )
+    # more fields
