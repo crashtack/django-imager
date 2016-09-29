01b17bee148837df3f6f804a06e3e30e9b83193e
James <canning.jimmy@gmail.com>
2016-09-29 16:51:39 -0700

    Forgot to save

--- imager_images/views.py
+++ imager_images/views.py
@@ -7,7 +7,6 @@ def library_view(request):
     current_user = User.objects.filter(username=request.user).first()
     images = current_user.photos.all()
     albums = current_user.albums.all()
-    thing = "astritn"
-    context = {'images': images, 'albums': albums, 'thing': thing}
+    context = {'images': images, 'albums': albums}
 
     return render(request, 'imager_images/library.html', context)
