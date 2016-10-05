09ecd3a73187996f3b4b1618351d2bf68e61edee
crashtack <crashtack@gmail.com>
2016-10-05 00:03:40 -0700

    trying to get emailworking

--- imagersite/production.py
+++ imagersite/production.py
@@ -10,6 +10,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 DEFAULT_FROM_EMAIL = '401.imagersite@gmail.com'
 SERVER_EMAIL = '401.imagersite@gmail.com'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = '401.imagersite@gmail.com'
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
 DATABASES = {
     'default': dj_database_url.config(default=os.environ['DATABASE_URL'])
