a6c5ce62c8e64efd0238ba0b1cf2e50a88dda083
crashtack <crashtack@gmail.com>
2016-10-05 00:06:33 -0700

    trying to get emailworking

--- imagersite/production.py
+++ imagersite/production.py
@@ -4,7 +4,7 @@ import dj_database_url
 
 DEBUG = False
 TEMPLATE_DEBUG = False
-ALLOWED_HOSTS = ['ec2-54-70-2-170.us-west-2.compute.amazonaws.com',
+ALLOWED_HOSTS = ['ec2-52-33-25-11.us-west-2.compute.amazonaws.com',
                  'localhost']
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
