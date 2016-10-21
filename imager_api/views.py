2e75bef558d3c0d82c6c5e9d5564379c569681e0
crashtack <crashtack@gmail.com>
2016-10-21 11:05:00 -0700

    Secured api to only logged in users

--- imager_api/views.py
+++ imager_api/views.py
@@ -2,7 +2,7 @@ from imager_images.models import Photo, Album
 from imager_api.serializers import PhotoSerializer, AlbumSerializer
 from imager_api.serializers import UserSerializer
 from rest_framework import viewsets, permissions
-from imager_api.permissions import IsOwnerOrReadOnly
+from imager_api.permissions import IsOwnerOrReadOnly, IsOwner
 from django.contrib.auth.models import User
 
 
@@ -10,10 +10,12 @@ class PhotoViewSet(viewsets.ModelViewSet):
     """
     This viewset automatically provides 'list', 'create', 'retrieve',
     'update', and 'destroy' actions.
+    TODO: add query for only private photos if you logged in user
+          is not the owner
     """
     queryset = Photo.objects.all()
     serializer_class = PhotoSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+    permission_classes = (permissions.IsAuthenticated,
                           IsOwnerOrReadOnly,)
     lookup_field = 'photo_id'
 
@@ -28,7 +30,7 @@ class AlbumViewSet(viewsets.ModelViewSet):
     """
     queryset = Album.objects.all()
     serializer_class = AlbumSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+    permission_classes = (permissions.IsAuthenticated,
                           IsOwnerOrReadOnly,)
     # lookup_field = 'photo_id'
 
@@ -40,6 +42,7 @@ class UserViewSet(viewsets.ReadOnlyModelViewSet):
     """
     Automatically provides 'list' and 'detail' views
     """
-    permissions_classes = (permissions.IsAuthenticated)
+    permissions_classes = (permissions.IsAuthenticated,
+                           IsOwner,)
     queryset = User.objects.all()
     serializer_class = UserSerializer
